68d0cd0a3f34d6b2caf1c7f665c31c785f15d427
brucelee2000 <brucelee2000@tp.edu.tw>
2015-02-01 15:24:25 -0800

    Parse setup and class creation and savings.

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize Parse
+        Parse.setApplicationId("JAQo5BNVtoi5aKJ7asfrJzonyrbKGrZjCWEMt4yD", clientKey: "STBZzmihMUuWliag8zOz1z0yKm327HyNh9SbGHum")
+        
+        // The `PFObject` class is a local representation of data persisted to the Parse cloud.
+        // Create a class with the name "score"
+        var score = PFObject(className: "score")
+        
+        // Create a property "name" of class "score"
+        score.setObject("Rob", forKey: "name")
+        score.setObject(23, forKey: "points")
+        
+        // Save the class into the cloud
+        score.saveInBackgroundWithBlock { (success:Bool!, error:NSError!) -> Void in
+            if success == true {
+                println("Score created with ID: \(score.objectId)")
+            } else {
+                println(error)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
